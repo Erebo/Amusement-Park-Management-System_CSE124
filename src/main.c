21f51b111bce0b3f1bd29aab388b7aa9d7c360a0
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-12-04 17:10:58 +0600

    Water Kingdom welcoming message added

--- src/main.c
+++ src/main.c
@@ -2,28 +2,11 @@
 
 void printDecember2025Calendar();
 int chooseDate2025();
-void showOffer2025(int date);   // <-- ADD THIS LINE
+void showOffer2025(int date);
+void fantasyKingdomWelcoming();
+void waterKingdomWelcoming();
 
 int main() {
-    printf(" __        __   _                            \n");
-    printf(" \\ \\      / /__| | ___ ___  _ __ ___   ___  \n");
-    printf("  \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ \n");
-    printf("   \\ V  V /  __/ | (_| (_) | | | | | |  __/ \n");
-    printf("    \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \n");
-    printf("                                             \n");
-
-    printf("  _____    \n");
-    printf(" |_   _|__  \n");
-    printf("   | |/ _ \\ \n");
-    printf("   | | (_) |\n");
-    printf("   |_|\\___/ \n");
-    printf("            \n");
-    printf("  _____          _                    _  __  _                         \n");
-    printf(" |  ___|_ _ _ __ | |_ __ _ ___ _   _  | |/ / (_)_ __   __ _  __| | ___  _ __ ___  \n");
-    printf(" | |_ / _` | '_ \\| __/ _` / __| | | | | ' /  | | '_ \\ / _` |/ _` |/ _ \\| '_ ` _ \\ \n");
-    printf(" |  _| (_| | | | | || (_| \\__ \\ |_| | | . \\  | | | | | (_| | (_| | (_) | | | | | |\n");
-    printf(" |_|  \\__,_|_| |_|\\__\\__,_|___/\\__, | |_|\\_\\ |_|_| |_|\\__, |\\__,_|\\___/|_| |_| |_|\n");
-    printf("                               |___/                   |___/                       \n");
     printDecember2025Calendar();
 
     int selectedDate = chooseDate2025();
@@ -32,6 +15,23 @@ int main() {
 
     // Show the offer for that day
     showOffer2025(selectedDate);
+
+    printf("Where you want to go: \n");
+    printf("1) Fantasy Kingdom\n");
+    printf("2) Water kingdom\n");
+    printf("> ");
+    int parkSelection;
+    scanf("%d", &parkSelection);
+
+    switch (parkSelection)
+    {
+    case 1:
+        fantasyKingdomWelcoming();
+    case 2:
+        waterKingdomWelcoming();
+    default:
+        break;
+    }
 
     return 0;
 }
