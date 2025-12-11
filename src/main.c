4764a93afad2ed0b5ef12d733ee28634c7d66ecd
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-12-11 15:00:48 +0600

    user data set on a file with serial number added

--- src/main.c
+++ src/main.c
@@ -13,24 +13,76 @@ int main() {
 
     printf("\nYou selected: December %d, 2025\n", selectedDate);
 
-    // Show the offer for that day
     showOffer2025(selectedDate);
 
-    printf("Where you want to go: \n");
+    char userName[100];
+    int userAge;
+    float userHeight, userWeight;
+    printf("Enter you name: ");
+    scanf(" %[^\n]", &userName);
+    printf("Enter your age: ");
+    scanf("%d", &userAge);
+    printf("Enter your height (in ft.): ");
+    scanf("%f", &userHeight);
+    printf("Enter your weight(in kg): ");
+    scanf("%f", &userWeight);
+
+    FILE *userDataForWriting, *userDataForReading;
+
+    int lastSerial = 0; 
+
+    userDataForReading = fopen("userData.txt", "r");
+
+    if (userDataForReading != NULL)
+    {
+        char line[1000];
+
+        while (fgets(line, sizeof(line), userDataForReading))
+        {
+            int serial;
+            if (sscanf(line, "serial: %d", &serial) == 1)
+            {
+                lastSerial = serial; 
+            }
+        }
+
+        fclose(userDataForReading);
+    }
+
+    int newSerial = lastSerial + 1;
+
+    userDataForWriting = fopen("userData.txt", "a");
+    fprintf(userDataForWriting, "serial: %d ", newSerial);
+    fprintf(userDataForWriting, "name: %s ", userName);
+    fprintf(userDataForWriting, "age: %d ", userAge);
+    fprintf(userDataForWriting, "height: %.2f ", userHeight);
+    fprintf(userDataForWriting, "weight: %.2f\n", userWeight);
+    fclose(userDataForWriting);
+
+    userDataForReading = fopen("userData.txt", "r");
+    char data[1000];
+    fscanf(userDataForReading, "%[^\n]", data);
+    printf("%s\n", data);
+    fclose(userDataForReading);
+
+    printf("Welcome %s, Where you want to go: \n", userName);
     printf("1) Fantasy Kingdom\n");
     printf("2) Water kingdom\n");
-    printf("> ");
     int parkSelection;
-    scanf("%d", &parkSelection);
+    chooseOptionAgain:
+        printf("> ");
+        scanf("%d", &parkSelection);
 
-    switch (parkSelection)
-    {
+    switch (parkSelection) {
     case 1:
         fantasyKingdomWelcoming();
+        break;
     case 2:
         waterKingdomWelcoming();
-    default:
         break;
+    default:
+        printf("Invalid option\n");
+        goto chooseOptionAgain;
     }
 
     return 0;
